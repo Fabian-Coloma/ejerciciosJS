51dffd8f22d6fc4b39dbacc3a833747ed7f5177a
Fabian Coloma <fabianaca17.123@gmail.com>
2026-06-15 11:32:15 -0500

    meconfundi

--- scripts/ejercicio3.js
+++ scripts/ejercicio3.js
@@ -8,3 +8,28 @@ if (vidaHeroe < 10) {
 } else {
     alert("Sobreviviste");
 }
+
+
+
+let nivel = 28; 
+let honor = 120; 
+let tienePaseVIP = true; 
+
+
+if (nivel >= 20 && (honor === 100 || tienePaseVIP === true)) {
+    alert("¡Acceso CONCEDIDO!");
+} else {
+    alert("Acceso DENEGADO.");
+}
+
+
+
+let a = 5;
+let b = 10;
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+alert("La cuenta A ahora tiene: " + a + " y la cuenta B tiene: " + b);
+
